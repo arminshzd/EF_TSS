ed122bba0bc5bdff8f6290f18bf638dbf2a29115
Armin Shayesteh Zadeh <67717991+arminshzd@users.noreply.github.com>
2023-11-04 17:59:20 -0500

    Update EF_TSS.py
    Fixed bug in periodic table.

--- EF_TSS.py
+++ EF_TSS.py
@@ -45,7 +45,7 @@ class EF_TSS:
 
         self.num_moving_atoms = 3*(self.N - (-sum(self.periphery)))
 
-        self.atom_dict_r = {1 : "H", 2  : "He", 3  : "Li", 4  : "Be", 5  : "Be", 6  : "C", 7  : "N", 8  : "O", 9  : "F", 10 : "Ne", 11 : "Na",\
+        self.atom_dict_r = {1 : "H", 2  : "He", 3  : "Li", 4  : "Be", 5  : "B", 6  : "C", 7  : "N", 8  : "O", 9  : "F", 10 : "Ne", 11 : "Na",\
                        12 : "Mg", 13 : "Al", 14 : "Si", 15 : "P", 16 : "Si", 17 : "Cl", 18 : "Ar", 19 : "K", 20 : "Ca", 21 : "Sc", 22 : "Ti",\
                        23 : "V", 24 : "Cr", 25 : "Mn", 26 : "Fe", 27 : "Co", 28 : "Ni", 29 : "Cu", 30 : "Zn", 31 : "Ga", 32 : "Ge", 33 : "As",\
                        34 : "Se", 35 : "Br", 36 : "Kr", 37 : "Rb", 38 : "Sr", 39 : "Y", 40 : "Zr", 41 : "Nb", 42 : "Mo", 43 : "Tc", 44 : "Ru",\
