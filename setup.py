c01c37e0ae9d9b329a6af44dfc86691041d1acee
Armin Shayesteh Zadeh <arminshzd@gmail.com>
2025-06-22 19:22:33 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="ef_tss",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "numpy>=2.0.1",
+        "scipy>=1.13.1",
+    ],
+    python_requires='>=3.9',
 )
